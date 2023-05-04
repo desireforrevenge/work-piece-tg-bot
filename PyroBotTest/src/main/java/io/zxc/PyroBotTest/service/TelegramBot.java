54a813ce14ddcbe692df0545e7e8227d55e30932
IIIyIIIa <raftalya@inbox.ru>
2023-05-04 11:54:37 +0300

    First

---
+++ PyroBotTest/src/main/java/io/zxc/PyroBotTest/service/TelegramBot.java
@@ -0,0 +1,61 @@
+package io.zxc.PyroBotTest.service;
+
+import io.zxc.PyroBotTest.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "Начать":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default: startCommandReceived(chatId,  "Sorry, command was not recognized");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name;
+
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException telegramApiException) {
+            //logs
+        }
+    }
+}
---
+++ PyroBotTest/src/test/java/io/zxc/PyroBotTest/PyroBotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zxc.PyroBotTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyroBotTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
